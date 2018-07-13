03e7e5bd16dcf930f67e2028c195d20a35eac754
Alekay <alekaymusic@gmail.com>
2018-07-12 20:24:39 -0400

    fix formatting

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
-  console.log('connected');
 
   function classToggle() {
     const navs = document.querySelectorAll('.navbar-items')
     
     navs.forEach(nav => nav.classList.toggle('navbar-toggleShow'));
   }
-  
+
   document.querySelector('.navbar-link-toggle')
     .addEventListener('click', classToggle);
+
 });
