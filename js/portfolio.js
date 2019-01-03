1a6fb6e94575b8a7d25ce2d68ccf598070fff4ca
Alexander Archer <alekay@Alexanders-MacBook-Pro.local>
2019-01-02 20:28:05 -0500

    add scroll functions

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,12 +1,34 @@
-// wait for DOM content to load
 document.addEventListener("DOMContentLoaded", function(){
 
-function classToggle() {
-  const navs = document.querySelectorAll('.navbar-items')
+  // navbar toggler
+    function classToggle() {
+      const navs = document.querySelectorAll('.navbar-items')
+      
+      navs.forEach(nav => nav.classList.toggle('navbar-toggleShow'));
+    }
+  
+    document.querySelector('.navbar-link-toggle').addEventListener('click', classToggle);
+  
+  // header changes when scroll
+  window.onscroll = () => {
+    scrollBar();
+    headerScrollFunc();
+  };
+  
+  headerScrollFunc = () =>{
+    if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+      document.getElementById('header box').className = 'headerScroll';
+    } else {
+      document.getElementById('header box').className = 'header box';
+    }
+  }
+  
+  // Scroll Bar
+  scrollBar = () => {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+  } 
     
-  navs.forEach(nav => nav.classList.toggle('navbar-toggleShow'));
-}
-
-document.querySelector('.navbar-link-toggle').addEventListener('click', classToggle);
-
 });
